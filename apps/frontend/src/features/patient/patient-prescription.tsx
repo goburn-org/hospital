ca2ba0ca5eea9af89ff01060f3e3367662cacda1
Chandrakumar <17010408+dreamerchandra@users.noreply.github.com>
2024-11-29 10:21:30 +0530

    feat: prescription ui changes

--- apps/frontend/src/features/patient/patient-prescription.tsx
+++ apps/frontend/src/features/patient/patient-prescription.tsx
@@ -15,6 +15,7 @@ import { FormAutoCompleteInput } from '../../component/form/form-auto-complete-i
 import { FormInput } from '../../component/form/form-input';
 import PageLoading from '../../component/page-loader';
 import { CustomTable } from '../../component/table';
+import Tooltip from '../../component/tooltip';
 import { TIMER_S, useTimer } from '../../utils/use-timer';
 import { useProductQuery } from '../product/use-product-query';
 import {
@@ -40,6 +41,15 @@ const ShowPrescription = ({
       {
         accessorKey: 'generic',
         header: 'Generic',
+        Cell: ({ renderedCellValue }) => {
+          return (
+            <Tooltip text={renderedCellValue}>
+              <div className="w-[10ch] overflow-hidden text-ellipsis">
+                {renderedCellValue}
+              </div>
+            </Tooltip>
+          );
+        },
       },
       {
         accessorKey: 'frequency',
@@ -130,7 +140,7 @@ const Form = ({
     <div className="flex flex-col gap-4">
       <FormProvider {...formProvider}>
         <form
-          className="bg-slate-50 p-4 rounded-lg"
+          className=" p-4 rounded-lg"
           onSubmit={formProvider.handleSubmit((data) => {
             if (Number.isFinite(editId)) {
               formProvider.reset();
@@ -145,165 +155,185 @@ const Form = ({
               return;
             }
             setList([...list, { ...data, idx: list.length }]);
+            formProvider.reset();
           })}
         >
-          <div className="mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-4">
-              <FormAutoCompleteInput<CreatePatientPrescriptionForm>
-                id="medicineName"
-                labelName="Item Name"
-                placeholder="Paracetamol"
-                isRequired
-                options={data?.data.map((d) => ({ id: d.id, label: d.name }))}
-                onEnter={(id) => {
-                  const product = data?.data.find((d) => d.id === id);
-                  if (!product) {
-                    return;
-                  }
-                  formProvider.setValue('generic', product?.genericName);
-                  document.getElementById('dosage')?.focus();
-                }}
-              />
-            </div>
-            <div className="sm:col-span-2">
-              <FormInput<CreatePatientPrescriptionForm>
-                autoComplete="off"
-                id="generic"
-                labelName="Generic"
-                type="text"
-                placeholder="Paracetamol"
-              />
-            </div>
-            <div className="sm:col-span-2">
-              <FormInput<CreatePatientPrescriptionForm>
-                autoComplete="off"
-                id="dosage"
-                labelName="Dosage"
-                type="text"
-                placeholder="500mg"
-              />
-            </div>
-            <div className="sm:col-span-2">
-              <FormInput<CreatePatientPrescriptionForm>
-                autoComplete="off"
-                id="frequency"
-                labelName="Frequency"
-                type="text"
-                placeholder="1-0-1"
-              />
-            </div>
-            <div className="sm:col-span-2">
-              <FormInput<CreatePatientPrescriptionForm>
-                autoComplete="off"
-                id="duration"
-                labelName="Duration"
-                type="text"
-                placeholder="30 days"
-              />
-            </div>
-            <div className="sm:col-span-2">
-              <FormInput<CreatePatientPrescriptionForm>
-                autoComplete="off"
-                id="form"
-                labelName="Form"
-                type="text"
-                placeholder="Tablet"
-              />
-            </div>
-            <div className="sm:col-span-2">
-              <FormInput<CreatePatientPrescriptionForm>
-                autoComplete="off"
-                id="instruction"
-                labelName="Instruction"
-                type="text"
-                placeholder="Before meal"
-              />
-            </div>
-            <div className="sm:col-span-2">
-              <FormInput<CreatePatientPrescriptionForm>
-                autoComplete="off"
-                id="quantity"
-                labelName="Quantity"
-                type="text"
-                placeholder="1"
-              />
-            </div>
-          </div>
-          <div className="flex items-center flex-row-reverse gap-x-6 p-4">
-            <button type="submit" className="btn-primary">
-              {typeof editId === 'number' ? 'Edit' : 'Add'}
-            </button>
-            <button
-              type="reset"
-              className={
-                typeof editId === 'number'
-                  ? 'btn-text !text-red-500'
-                  : 'btn-text'
-              }
-              onClick={(e) => {
-                if (Number.isFinite(editId)) {
+          <div className="flex flex-col-reverse gap-12 lg:flex-row">
+            <div className="w-full lg:w-1/2">
+              <ShowPrescription
+                data={list}
+                setEdit={useRef((idx: number) => {
                   formProvider.clearErrors();
-                  formProvider.reset();
-                  e.preventDefault();
-                  setList((l) => l.filter((_, idx) => idx !== editId));
-                  setEditId(null);
-                }
-              }}
-            >
-              {typeof editId === 'number' ? 'Delete' : 'clear'}
-            </button>
+                  setEditId(idx);
+                  setList((l) => {
+                    const old = l[idx];
+                    if (!old) {
+                      return l;
+                    }
+                    Object.entries(old).map(([k, v]) =>
+                      formProvider.setValue(k as any, v),
+                    );
+                    return l;
+                  });
+                })}
+              />
+              <div className="w-full lg:w-1/2">
+                <button
+                  className="btn-primary"
+                  onClick={async (e) => {
+                    e.preventDefault();
+                    await mutateAsync({
+                      patientId,
+                      prescription: list,
+                      visitId,
+                    });
+                    toast.success('Prescription Saved');
+                    setSaved(true);
+                    start(() => {
+                      setSaved(false);
+                    });
+                  }}
+                >
+                  {saved ? (
+                    <>
+                      <CheckIcon className="w-5 h-5" width={24} />
+                      Saved
+                    </>
+                  ) : (
+                    'Create Prescription'
+                  )}
+                </button>
+              </div>
+            </div>
+            <div className="w-full lg:w-1/2 bg-slate-50 p-4">
+              <div className="mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="col-span-6 2xl:col-span-4">
+                  <FormAutoCompleteInput<CreatePatientPrescriptionForm>
+                    id="medicineName"
+                    labelName="Item Name"
+                    placeholder="Paracetamol"
+                    isRequired
+                    options={data?.data.map((d) => ({
+                      id: d.id,
+                      label: d.name,
+                    }))}
+                    onEnter={(id) => {
+                      const product = data?.data.find((d) => d.id === id);
+                      if (!product) {
+                        return;
+                      }
+                      formProvider.setValue('generic', product?.genericName);
+                      document.getElementById('dosage')?.focus();
+                    }}
+                  />
+                </div>
+                <div className="col-span-6 2xl:col-span-2">
+                  <FormInput<CreatePatientPrescriptionForm>
+                    autoComplete="off"
+                    id="generic"
+                    labelName="Generic"
+                    type="text"
+                    placeholder="Paracetamol"
+                  />
+                </div>
+                <div className="col-span-2">
+                  <FormInput<CreatePatientPrescriptionForm>
+                    autoComplete="off"
+                    id="dosage"
+                    labelName="Dosage"
+                    type="text"
+                    placeholder="500mg"
+                  />
+                </div>
+                <div className="col-span-2">
+                  <FormInput<CreatePatientPrescriptionForm>
+                    autoComplete="off"
+                    id="frequency"
+                    labelName="Frequency"
+                    type="text"
+                    placeholder="1-0-1"
+                  />
+                </div>
+                <div className="col-span-2">
+                  <FormInput<CreatePatientPrescriptionForm>
+                    autoComplete="off"
+                    id="duration"
+                    labelName="Duration"
+                    type="text"
+                    placeholder="30 days"
+                  />
+                </div>
+                <div className="col-span-2">
+                  <FormInput<CreatePatientPrescriptionForm>
+                    autoComplete="off"
+                    id="form"
+                    labelName="Form"
+                    type="text"
+                    placeholder="Tablet"
+                  />
+                </div>
+                <div className="col-span-2">
+                  <FormInput<CreatePatientPrescriptionForm>
+                    autoComplete="off"
+                    id="instruction"
+                    labelName="Instruction"
+                    type="text"
+                    placeholder="Before meal"
+                  />
+                </div>
+                <div className="col-span-2">
+                  <FormInput<CreatePatientPrescriptionForm>
+                    autoComplete="off"
+                    id="quantity"
+                    labelName="Quantity"
+                    type="text"
+                    placeholder="1"
+                  />
+                </div>
+              </div>
+              <div className="flex justify-between mt-8">
+                {editId ? (
+                  <button
+                    type="reset"
+                    onClick={(e) => {
+                      if (Number.isFinite(editId)) {
+                        formProvider.clearErrors();
+                        formProvider.reset();
+                        e.preventDefault();
+                        setList((l) => l.filter((_, idx) => idx !== editId));
+                        setEditId(null);
+                      }
+                    }}
+                    className="btn-danger"
+                  >
+                    Delete
+                  </button>
+                ) : (
+                  <>.</>
+                )}
+                <div className="flex flex-row-reverse gap-2">
+                  <button type="submit" className="btn-primary">
+                    {typeof editId === 'number' ? 'Edit' : 'Add'}
+                  </button>
+                  <button
+                    type="reset"
+                    className={
+                      typeof editId === 'number'
+                        ? 'btn-text !text-red-500'
+                        : 'btn-text'
+                    }
+                    onClick={(e) => {
+                      formProvider.reset();
+                    }}
+                  >
+                    Clear
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </form>
       </FormProvider>
-      <div className="flex justify-end items-center gap-4 w-[65vw]">
-        <button
-          className="btn-primary"
-          onClick={async (e) => {
-            e.preventDefault();
-            await mutateAsync({
-              patientId,
-              prescription: list,
-              visitId,
-            });
-            toast.success('Prescription Saved');
-            setSaved(true);
-            start(() => {
-              setSaved(false);
-            });
-          }}
-        >
-          {saved ? (
-            <>
-              <CheckIcon className="w-5 h-5" width={24} />
-              Saved
-            </>
-          ) : (
-            'Create Prescription'
-          )}
-        </button>
-      </div>
-      {
-        <div>
-          <h2 className="text-xl font-bold">Prescription List</h2>
-          <ShowPrescription
-            data={list}
-            setEdit={useRef((idx: number) => {
-              formProvider.clearErrors();
-              setEditId(idx);
-              setList((l) => {
-                const old = l[idx];
-                if (!old) {
-                  return l;
-                }
-                Object.entries(old).map(([k, v]) =>
-                  formProvider.setValue(k as any, v),
-                );
-                return l;
-              });
-            })}
-          />
-        </div>
-      }
     </div>
   );
 };
@@ -320,7 +350,7 @@ export const PatientPrescription = () => {
     return <div>Visit Not not found</div>;
   }
   return (
-    <div className="w-[100vw] sm:w-[85vw] 2xl:w-[70vw]">
+    <div className="w-[100vw] sm:w-[calc(100vw-133.8px)]">
       <Form
         patientId={patientId}
         visitId={visitId}
